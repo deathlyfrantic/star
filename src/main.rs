2d348999932baef8b8b85a337335cc7d11885f0f
Zandr Martin <git@zandr.me>
2019-02-15 07:13:59 -0500

    rustfmt changes

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,8 @@ fn main() {
                 .help("Specify UI height in lines (including prompt)")
                 .takes_value(true)
                 .default_value("21"),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("search")
                 .short("s")
                 .long("search")
@@ -55,12 +56,14 @@ fn main() {
                 .takes_value(true)
                 .default_value("")
                 .hide_default_value(true),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("version")
                 .short("v")
                 .long("version")
                 .help("Output version information then exit"),
-        ).get_matches();
+        )
+        .get_matches();
 
     if matches.is_present("version") {
         println!("0.1.0");
