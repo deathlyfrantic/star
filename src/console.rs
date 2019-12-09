3c23f36de423b476f299e5a5f81c2c84b5f09dd2
Zandr Martin <git@zandr.me>
2019-12-09 07:38:40 -0500

    Revert "use termion's built-in `terminal_size` function"
    This reverts commit 3f4188faa328f5770ba62870a850cdde3b55b048.
    
    Unfortunately Termion's `terminal_size()` function requests the size of
    whatever stdout is, so if you pipe Star's output to another program, or
    try to write it to a file, you'll get an "Inappropriate ioctl for
    device" error. The solution is to go back to our old `terminal_size()`
    implementation, which is shamelessly ripped off of Termion's, but takes
    a FD parameter. We can then provide the console's FD, which is not
    necessarily stdout, to avoid this error.

--- src/console.rs
+++ src/console.rs
@@ -1,9 +1,11 @@
+use libc::{c_ushort, ioctl, TIOCGWINSZ};
 use std::{
     fs::File,
     io::{self, Write},
-    os::unix::io::AsRawFd,
+    mem,
+    os::unix::io::{AsRawFd, RawFd},
 };
-use termion::{self, cursor, terminal_size};
+use termion::{self, cursor};
 use termios::{cfmakeraw, tcsetattr, Termios, ECHO, ICANON, TCSANOW};
 
 #[derive(Debug)]
@@ -17,7 +19,7 @@ pub struct Console {
 impl Console {
     pub fn new() -> io::Result<Self> {
         let tty = termion::get_tty()?;
-        let (width, height) = terminal_size()?;
+        let (width, height) = terminal_size(tty.as_raw_fd())?;
         let mut termios = Termios::from_fd(tty.as_raw_fd())?;
         let original_state = termios;
 
@@ -43,5 +45,24 @@ impl Console {
 impl Drop for Console {
     fn drop(&mut self) {
         tcsetattr(self.tty.as_raw_fd(), TCSANOW, &self.original_state).unwrap();
+    }
+}
+
+#[repr(C)]
+struct TermSize {
+    row: c_ushort,
+    col: c_ushort,
+    _x: c_ushort,
+    _y: c_ushort,
+}
+
+fn terminal_size(fd: RawFd) -> io::Result<(u16, u16)> {
+    unsafe {
+        let mut size: TermSize = mem::zeroed();
+        if ioctl(fd, TIOCGWINSZ, &mut size as *mut _) == -1 {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok((size.col as u16, size.row as u16))
+        }
     }
 }
